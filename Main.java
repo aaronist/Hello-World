182e6bd447697fd1aff1333def128736b2db89f4
aaronist <66182368+aaronist@users.noreply.github.com>
2020-08-12 18:44:36 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Aaron Blume.");
+		System.out.println("This will be my senior year in high school.");
+		System.out.println("I have already learned a little bit of Java.");
+		System.out.println("In terms of college, I hope to attend a UC.");
+		System.out.println("Writing my college essays has not been fun.");
+	}
+}
+
